2d83759d15d72435b480c0d52ce311e46897a4b7
byung ju choi <bjc30849@gmail.com>
2022-01-11 16:47:53 +0900

    update

--- src/atom.tsx
+++ src/atom.tsx
@@ -9,6 +9,7 @@ interface IToDoProps  {
     //키가 string value가 string[]을 가진다는 것을 표현
 }
 export const toDoState = atom<IToDoProps>({
+    //IToDoProps object를 배열의 원소로 가진다
     key:"todo",
     default:{
         "To Do":[],
